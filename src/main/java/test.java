77da0ba9dbea44f25f16d58b083dbf92b3e93c03
AceHCL <1782934084@qq.com>
2021-07-31 00:13:27 +0800

    test

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * 描述:
+ *
+ * @author ace-huang
+ * @create 2021-07-31 12:10 上午
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
